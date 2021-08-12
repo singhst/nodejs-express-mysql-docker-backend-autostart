94e74b28a6c54eb175bd5e577a4e78da75cd1d75
singhst <singhst@users.noreply.github.com>
2021-08-13 00:48:55 +0800

    rename testing data

--- testing-and-data-model.sql
+++ testing-and-data-model.sql
@@ -18,6 +18,6 @@ CREATE TABLE `plan_info` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 insert  into `plan_info`(`plan_id`,`plan_name`,`general`,`specialist`,`physiotherapy`,`SYMPTOM_n`,`plan_desc`,`plan_month_price`) values
-(1,'Standard Plan',1,0,0,0,'This is standard plan description.',0),
-(2,'Premium Plan',1,1,1,1,'This is premium plan description',388);
+(1,'standard',1,0,0,0,'this is standard plan description.',0),
+(2,'premium',1,1,1,1,'this is premium plan description',388);
 
