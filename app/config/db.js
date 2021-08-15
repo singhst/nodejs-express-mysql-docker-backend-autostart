b24443d88a6e1e57c56999579fb60e13243963a8
singhst <singhst@users.noreply.github.com>
2021-08-15 16:22:29 +0800

    nodejs>solved mysql container connect issue, changed hostname

--- app/config/db.js
+++ app/config/db.js
@@ -7,7 +7,13 @@
 const mysql = require('mysql');
 
 var con = mysql.createConnection({
-	host: 'localhost',
+	//Why `db` can be ok to be the IP address to find the MySQL server?
+	//Ans: 
+	//=> Each container for a service joins the default network and is both reachable by other containers on that network, and discoverable by them at a hostname identical to the container name.
+	//=> docker has a default DNS server, it takes the service/container name in "docker-compose.yaml" as the hostname (which can be mapped to container's IP address).
+	//ref: https://docs.docker.com/compose/networking/ 
+	// host: 'localhost',
+	host: 'db',	
 	user: 'root',
 	password: '00000000',
 	database: 'plan',
